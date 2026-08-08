3b4652e05a96e88c98afb1ef4907e8bbc2e64a4a
Morteza Kouhsar <m.kouhsar@exeter.ac.uk>
2026-08-08 02:45:25 +0100

    Updated

--- QC/5_CheckEthnicity.sh
+++ QC/5_CheckEthnicity.sh
@@ -12,9 +12,21 @@ plink --bfile ${FilePrefix}_QC_final --update-name Ethnicity/${FilePrefix}_Updat
 echo
 echo "[INFO} change variant ids to chr:bp in reference data"
 echo
+
 RefGenome_file=$(basename $RefGenome_binary)
-awk '{if ($1 != 0) print $2,"chr"$1":"$4}' ${RefGenome_binary}.bim > Ethnicity/${RefGenome_file}_UpdateVariantsFormat.txt
-plink --bfile $RefGenome_binary --update-name Ethnicity/${RefGenome_file}_UpdateVariantsFormat.txt --biallelic-only --allow-extra-chr --make-bed --out Ethnicity/${RefGenome_file}_Updated
+
+plink --bfile $RefGenome_binary --allow-extra-chr --chr 1-22 --make-bed --out Ethnicity/${RefGenome_file}_1_22
+# 1. Extract duplicate variant IDs (skipping their first occurrence)
+awk '{print $2}' ${RefGenome_binary}.bim | awk 'seen[$0]++' > Ethnicity/${RefGenome_file}_dup_variants_to_remove.txt
+
+# 2. Exclude the duplicate instances
+plink --bfile Ethnicity/${RefGenome_file}_1_22 \
+      --exclude Ethnicity/${RefGenome_file}_dup_variants_to_remove.txt \
+      --make-bed \
+      --out Ethnicity/${RefGenome_file}_noDup
+
+awk '{if ($1 != 0) print $2,"chr"$1":"$4}' Ethnicity/${RefGenome_file}_noDup.bim > Ethnicity/${RefGenome_file}_UpdateVariantsFormat.txt
+plink --bfile Ethnicity/${RefGenome_file}_noDup --update-name Ethnicity/${RefGenome_file}_UpdateVariantsFormat.txt --biallelic-only --make-bed --out Ethnicity/${RefGenome_file}_Updated
 RefGenome_binary="Ethnicity/${RefGenome_file}_Updated"
 # first merge with 1000 genomes and filter variants to those in common
 # need to test initially in case of error with triallelic variants
@@ -24,16 +36,54 @@ echo "[INFO] Merging the data with ref genome"
 echo
 plink --bfile Ethnicity/${FilePrefix}_QC_final_1kgIDs --bmerge $RefGenome_binary --maf 0.1 --geno 0.1 --make-bed --out Ethnicity/${FilePrefix}_QC_final_mergedw1000G || true
 
-if [ -e Ethnicity/${FilePrefix}_QC_final_mergedw1000G*.missnp ]
+if [ -e Ethnicity/${FilePrefix}_QC_final_mergedw1000G-merge.missnp ]
 then
   echo
-  echo "[INFO} Removing problematic SNPs from the data..."
+  echo "[INFO] Problematic SNPs detected. Attempting to flip strands..."
   echo
-  plink --bfile Ethnicity/${FilePrefix}_QC_final_1kgIDs --exclude Ethnicity/${FilePrefix}_QC_final_mergedw1000G*.missnp --make-bed --out Ethnicity/${FilePrefix}_QC_final_1kgIDs.1
+  # Step 1: Try flipping the mismatched SNPs in your input data
+  plink --bfile Ethnicity/${FilePrefix}_QC_final_1kgIDs \
+        --flip Ethnicity/${FilePrefix}_QC_final_mergedw1000G-merge.missnp \
+        --make-bed \
+        --out Ethnicity/${FilePrefix}_QC_final_1kgIDs_flipped
+
   echo
-  echo "[INFO] Try to merge again"
+  echo "[INFO] Trying to merge again after flipping..."
   echo
-  plink --bfile Ethnicity/${FilePrefix}_QC_final_1kgIDs.1 --bmerge $RefGenome_binary --maf 0.1 --geno 0.1 --make-bed --out Ethnicity/${FilePrefix}_QC_final_mergedw1000G
+  plink --bfile Ethnicity/${FilePrefix}_QC_final_1kgIDs_flipped \
+        --bmerge $RefGenome_binary \
+        --maf 0.1 --geno 0.1 \
+        --make-bed \
+        --out Ethnicity/${FilePrefix}_QC_final_mergedw1000G || true
+
+  # Step 2: If it STILL fails, exclude the remaining stubborn SNPs from BOTH datasets
+  if [ -e Ethnicity/${FilePrefix}_QC_final_mergedw1000G-merge.missnp ]
+  then
+    echo
+    echo "[INFO] Flipping didn't resolve all SNPs. Excluding them from both datasets..."
+    echo
+    
+    # Exclude from input data
+    plink --bfile Ethnicity/${FilePrefix}_QC_final_1kgIDs_flipped \
+          --exclude Ethnicity/${FilePrefix}_QC_final_mergedw1000G-merge.missnp \
+          --make-bed \
+          --out Ethnicity/${FilePrefix}_QC_final_1kgIDs.1
+          
+    # Exclude from reference data
+    plink --bfile $RefGenome_binary \
+          --exclude Ethnicity/${FilePrefix}_QC_final_mergedw1000G-merge.missnp \
+          --make-bed \
+          --out Ethnicity/${RefGenome_file}_clean
+          
+    echo
+    echo "[INFO] Final attempt to merge..."
+    echo
+    plink --bfile Ethnicity/${FilePrefix}_QC_final_1kgIDs.1 \
+          --bmerge Ethnicity/${RefGenome_file}_clean \
+          --maf 0.1 --geno 0.1 \
+          --make-bed \
+          --out Ethnicity/${FilePrefix}_QC_final_mergedw1000G
+  fi
 fi
 
 echo "[INFO] LD prune"
